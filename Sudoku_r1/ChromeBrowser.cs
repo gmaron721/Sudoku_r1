58687cc5b76b1d069563229e24f42e9395839c98
gmaron721 <gmaron@protonmail.com>
2021-07-07 16:50:54 +0400

    Init

---
+++ Sudoku_r1/ChromeBrowser.cs
@@ -0,0 +1,106 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Sudoku_r1
+{
+    enum ChromeBrowserResult
+    {
+        OK,
+        BrowserPathError,
+        ProxyError,
+        UnknowError
+    }
+    class ChromeBrowser
+    {
+        private static string browserPath;
+        private static string driverPath;
+        private static ChromeBrowserResult GetChromeExecutableName()
+        {
+            try
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    browserPath = AppDomain.CurrentDomain.BaseDirectory + @"Resources\chrome-win\chrome.exe";
+                    driverPath = AppDomain.CurrentDomain.BaseDirectory + @"Resources\chrome-win";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    browserPath = AppDomain.CurrentDomain.BaseDirectory + @"Resources\chrome-linux\chrome";
+                    driverPath = AppDomain.CurrentDomain.BaseDirectory + @"Resources\chrome-linux";
+                }
+
+
+                if (System.IO.File.Exists(browserPath) && System.IO.Directory.Exists(driverPath))
+                {
+                    return ChromeBrowserResult.OK;
+                }
+                else
+                {
+                    Console.WriteLine("Paths not found");
+                    return ChromeBrowserResult.BrowserPathError;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ChromeBrowserResult.BrowserPathError;
+            }
+        }
+
+        public IWebDriver driver { get; private set; }
+        public ChromeBrowserResult CloseBrowser()
+        {
+            try
+            {
+                driver.Close();
+                driver.Quit();
+                return ChromeBrowserResult.OK;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ChromeBrowserResult.UnknowError;
+            }
+        }
+        public ChromeBrowserResult Init(string browserPath, string driverPath)
+        {
+            try
+            {
+                //Консоль
+                ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService(driverPath);
+                chromeDriverService.HideCommandPromptWindow = false;
+
+                //Браузер
+                ChromeOptions options = new ChromeOptions();
+                options.BinaryLocation = browserPath;
+
+                //options.AddArgument("headless");
+
+                //----------------
+                driver = new ChromeDriver(chromeDriverService, options);
+                //----------------
+
+                return ChromeBrowserResult.OK;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ChromeBrowserResult.UnknowError;
+            }
+        }
+
+        public ChromeBrowser()
+        {
+            if (GetChromeExecutableName().Equals(ChromeBrowserResult.OK))
+            {
+                Init(browserPath, driverPath);
+            }
+        }
+
+        //The End
+    }
+}
--- Sudoku_r1/Program.cs
+++ Sudoku_r1/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Sudoku_r1
 {
@@ -6,7 +15,146 @@ namespace Sudoku_r1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
         }
+
+        public async static void R1(string url)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);//Например https://grandgames.net/sudoku/id145319"
+            string html = await response.Content.ReadAsStringAsync();
+
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            string result = string.Empty;
+            var tables = doc.DocumentNode.SelectNodes("//table");
+
+            //remove unnecessary-------------------
+            tables.RemoveAt(0);
+            tables.RemoveAt(0);
+            for (int i = 0; i < 82; i++)
+            {
+                tables.RemoveAt(tables.Count - 1);
+            }
+            //------------------------------------
+
+            foreach (HtmlNode table in tables)
+            {
+                //Console.WriteLine("Found: " + table.Id);
+                foreach (HtmlNode row in table.SelectNodes("tr"))
+                {
+                    result += "row" + Environment.NewLine;
+                    foreach (HtmlNode cell in row.SelectNodes("th|td"))
+                    {
+                        result += "cell: " + cell.InnerText + Environment.NewLine;
+                    }
+
+                }
+            }
+
+            result = string.Join("", result.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
+            string regularExpressionPattern1 = @"1234(.*?)56789";
+            Regex regex = new Regex(regularExpressionPattern1, RegexOptions.Singleline);
+            MatchCollection collection = regex.Matches(result);
+
+            List<int?> list = new List<int?>();//оставить список на случай если понадобится исходная задача
+            foreach (Match c in collection)
+            {
+                if (string.IsNullOrEmpty(c.Groups[1].Value.ToString()))
+                {
+                    list.Add(null);
+                }
+                else
+                {
+                    list.Add(int.Parse(c.Groups[1].Value));
+                }
+            }
+
+
+            int?[,] a = new int?[9, 9];//int? Типы, допускающие значение NULL
+            //List-to-array[,]----------------------------
+            int iCount = 0;
+            foreach (var number in list)
+            {
+                a[iCount % 9, (int)(iCount / 9)] = number;
+                iCount++;
+            }
+            //--------------------------------------------
+
+            Grid grid = new Grid(a);
+            SudokoSolver ss = new SudokoSolver(grid);
+            ss.SolvePuzzle();
+
+            result = string.Empty;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    result += grid.Data[j, i] + " ";//Console.Write(grid.Data[j,i]+" ");
+                }
+                result += Environment.NewLine;//Console.WriteLine(); Console.WriteLine();
+            }
+
+            string nameID = url.Split('d')[3];
+            File.WriteAllText(nameID + ".txt", result);
+            client.Dispose();
+        }
+
+        public static Account account = new Account();
+        public class Account
+        {
+            public string login;
+            public string pass;
+        }
+        public static int ReadAccount()
+        {
+            try
+            {
+                using (StreamReader r = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + @"Resources\Account.json"))
+                {
+                    string json = r.ReadToEnd();
+                    account = JsonConvert.DeserializeObject<Account>(json);
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+
+        async static Task MainAsync(string[] args)
+        {
+            int readAccountResult = ReadAccount();
+            if (readAccountResult == 0)
+            {
+                ChromeBrowser b1 = new ChromeBrowser();
+                b1.driver.Navigate().GoToUrl("http://www.grandgames.net/");
+                Thread.Sleep(1000);
+                IWebElement query = b1.driver.FindElement(By.Id("lfshow"));
+                query.Click();
+                Thread.Sleep(2000);
+
+                b1.driver.FindElement(By.Name("login")).SendKeys(account.login);
+                b1.driver.FindElement(By.Name("pass")).SendKeys(account.pass);
+                Thread.Sleep(500);
+                b1.driver.FindElement(By.Name("pass")).SendKeys(Keys.Enter);
+                Thread.Sleep(1000);
+                b1.driver.Navigate().GoToUrl("http://www.grandgames.net/sudoku");
+
+                var tt = b1.driver.FindElement(By.ClassName("pages")).Text;
+            }
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+        //----------THE END
     }
 }
