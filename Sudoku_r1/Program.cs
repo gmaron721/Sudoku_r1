30755bf56c5ee7ceeb42f7f907d47211c9d9caad
gmaron721 <gmaron@protonmail.com>
2021-07-07 16:45:11 +0400

    Добавьте файлы проекта.

---
+++ Sudoku_r1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sudoku_r1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
